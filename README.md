a3329a87130627fde5bd7f018a6164f109276def
thanhluan <s2bnl2s@gmail.com>
2025-04-04 11:58:04 +0700

    Tạo mới Flutter Demo

---
+++ README.md
@@ -0,0 +1,8 @@
+# shopapp
+
+A new Flutter project.
+
+## Shopapp
+Flutter 3.27.2
+Android 35
+IOS 12
---
+++ android/app/src/main/java/vn/luan/shopapp/MainActivity.java
@@ -0,0 +1,6 @@
+package vn.luan.shopapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
